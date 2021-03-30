d4ad97bf11d1c607c37498ad052c1f97549e3532
Matthew Cave <mattcave623@gmail.com>
2021-03-30 18:08:44 +0100

    Add Domain types: Pokemon class and Habitat enum.

---
+++ Pokedex.Domain/Pokemon.cs
@@ -0,0 +1,43 @@
+﻿using Pokedex.Domain.Enums;
+using System;
+
+namespace Pokedex.Domain
+{
+    public class Pokemon: IEquatable<Pokemon>
+    {
+        public Pokemon(string name, string description, Habitat habitat, bool isLegendary)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name", "Name of Pokemon cannot be null");
+
+            Name = name;
+            Description = description;
+            Habitat = habitat;
+            IsLegendary = isLegendary;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+        public Habitat Habitat { get; }
+        public bool IsLegendary { get; }
+
+        public bool Equals(Pokemon other)
+        {
+            if (other == null)
+                return false;
+            return Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Pokemon other = obj as Pokemon;
+
+            return Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
