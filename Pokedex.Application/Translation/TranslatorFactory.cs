08f50a911e57c7269fc069d7c5e577240f5a4b3d
Matthew Cave <mattcave623@gmail.com>
2021-04-04 16:05:34 +0100

    Implement TranslatorFactory and FunTranslator.

---
+++ Pokedex.Application/Translation/TranslatorFactory.cs
@@ -0,0 +1,26 @@
+﻿using Pokedex.Application.Interfaces;
+using Pokedex.Application.Translation.Dtos;
+using Pokedex.Domain;
+
+namespace Pokedex.Application.Translation
+{
+    public class TranslatorFactory : ITranslatorFactory
+    {
+        private static readonly string _caveHabitatName = "cave";
+        private readonly IReader<TranslationRequest, TranslationResult> _translationResultReader;
+
+        public TranslatorFactory(IReader<TranslationRequest, TranslationResult> translationResultReader)
+        {
+            _translationResultReader = translationResultReader;
+        }
+        public ITranslator CreateTranslator(Domain.Pokemon pokemon)
+        {
+            var translationType = TranslationType.Shakespeare;
+
+            if (pokemon.Habitat == _caveHabitatName || pokemon.IsLegendary)
+                translationType = TranslationType.Yoda;
+
+            return new FunTranslator(_translationResultReader, translationType);
+        }
+    }
+}
